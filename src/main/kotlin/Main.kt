a9a6a63c85e6b0f0e36dccd83a8e9ab0384b0ad2
ToothLonely <arsen.shirinov2004@gmail.com>
2025-02-01 01:27:41 +0300

    JP rename variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,8 @@ fun main() {
     val doc: Document =
         Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
             .get()
-    val asdfa: Elements = doc.select(".sc-14uz67c-0 .sc-2aegk7-0 .sc-2aegk7-1 .sc-2aegk7-2 ")
-    for (i in asdfa) {
-        println("${i.text()} \n")
+    val quotes: Elements = doc.select(".sc-14uz67c-0 .sc-2aegk7-0 .sc-2aegk7-1 .sc-2aegk7-2 ")
+    for (quote in quotes) {
+        println("${quote.text()} \n")
     }
 }
