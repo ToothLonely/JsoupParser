d34bd36a3162bbb628cecb8caf5078d70f872daf
ToothLonely <arsen.shirinov2004@gmail.com>
2025-02-01 01:25:51 +0300

    JP parse quotes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package org.example
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
 fun main() {
-    println("Hello World!")
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
+    val asdfa: Elements = doc.select(".sc-14uz67c-0 .sc-2aegk7-0 .sc-2aegk7-1 .sc-2aegk7-2 ")
+    for (i in asdfa) {
+        println("${i.text()} \n")
+    }
 }
